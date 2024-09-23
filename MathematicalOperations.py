9b1ba3159e39d03fd64b994aeddd6d7fd208c8f0
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 11:39:40 +0530

    Create MathematicalOperations.py

---
+++ MathematicalOperations.py
@@ -0,0 +1,8 @@
+#Take the inputs for end user and add basic mathematical operations
+a=int(input("Enter A Number"))
+b=int(input("Enter A Number"))
+print("Addition : ",a+b)
+print("subtraction : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
+print("Mod : ",a%b)
