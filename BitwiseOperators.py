a042086b6107c19aab2ddda061b8dc8970cb226e
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 14:50:19 +0530

    Create BitwiseOperators.py

---
+++ BitwiseOperators.py
@@ -0,0 +1,9 @@
+#Write a program to demonstrate Bitwise operators
+a=int(input("Enter First number :"))
+b=int(input("Enter Second number :"))
+print(a|b)
+print(a&b)
+print(~b)
+print(a^b)
+print(a>>b)
+print(a<<b)
