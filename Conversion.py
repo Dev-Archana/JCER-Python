853044266de2135a213f3bd993feccdd6945b0a8
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 14:41:18 +0530

    Create Conversion.py

---
+++ Conversion.py
@@ -0,0 +1,11 @@
+#Basic Conversion 
+a=int(input("Enter a number :"))
+b=int(input("Enter a number :"))
+print("Converting int to float :",float(a))
+print("Converting int to float :",float(b))
+print("Converting int to complex :",complex(b))
+print("Converting int to str :",str(b))
+l1=[1,'A',"XYZ",9.0]
+print(l1)
+print(tuple(l1))
+print(set(l1))
