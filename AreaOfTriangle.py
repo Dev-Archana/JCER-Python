68b3b0f05289dbace2b01f929f5b1290dd5c9555
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 11:20:56 +0530

    Create AreaOfTriangle.py

---
+++ AreaOfTriangle.py
@@ -0,0 +1,5 @@
+#Program to find the area of the triangle for the given base and height
+h=int(input("Enter Height of a triangle :"))
+b=int(input("Enter base of a triangle :"))
+area=1/2*h*b
+print("Area of Triangle: ",area)
