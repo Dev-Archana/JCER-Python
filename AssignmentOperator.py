c0febb52d44185228ce88404da0ad0a731cb7504
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 14:18:57 +0530

    Create AssignmentOperator.py

---
+++ AssignmentOperator.py
@@ -0,0 +1,17 @@
+#Write a program to demonstrate Assignment Operators in python Programming
+a=int(input("Enter A Number :"))#Normal Assignment 
+print(a)
+a+=10
+print(a)
+a-=10
+print(a)
+a*=10
+print(a)
+a/=10
+print(a)
+a%=10
+print(a)
+a//=10
+print(a)
+a**=10
+print(a)
