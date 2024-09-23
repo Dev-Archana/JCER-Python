c3aad1e107aa1d90f41f4314246cf89f13acf87e
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 11:03:48 +0530

    Create AreaOfReactangle.py

---
+++ AreaOfReactangle.py
@@ -0,0 +1,5 @@
+#Program to find the area of the rectangle for a given length and width
+length=int(input("Enter length of Reactangle : "))
+width=int(input("Enter width of Reactangle : "))
+area_rectangle=length*width
+print(area_rectangle)
