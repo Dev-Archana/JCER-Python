6494c1705f58dc3796d02b65b433ba0446a83643
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 12:26:02 +0530

    Create SwappingOfNumberWith3rdVariable.py

---
+++ SwappingOfNumberWith3rdVariable.py
@@ -0,0 +1,12 @@
+#Swapping of two number
+a=int(input("Enter A Number\n"))
+b=int(input("Enter A Number\n"))
+print("Before swapping")
+print("Value of A:",a)
+print("Value of B:",b)
+temp=a
+a=b
+b=temp
+print("After swapping")
+print("Value of A:",a)
+print("Value of B:",b)
