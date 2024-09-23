5e260344b0e821e121f16bcdf7fa4b53170cf020
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 12:38:57 +0530

    Create FindingType.py

---
+++ FindingType.py
@@ -0,0 +1,14 @@
+#Find type of given variable 
+a=10
+print(type(a))
+b=10.00
+print(type(b))
+c='A'
+print(type(c))
+d="Jain College"
+print(type(d))
+print(type(complex(a)))
+f=[1,"Hello","A",10.0]
+print(type(f))
+g=(1,"Hello","A",10.0)
+print(type(g))
