a32eb8b2c325f23f882f95f351bc785d34d506c4
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 12:36:58 +0530

    Create DataTypesInPython.py

---
+++ DataTypesInPython.py
@@ -0,0 +1,14 @@
+#demonstartion of datatypes in python
+a=10
+print(type(a))
+b=10.00
+print(type(b))
+c='A'
+print(type(c))
+d="Jain College"
+print(type(d))
+print(type(complex(a)))
+f=[1,"Hello","A",10.0]
+print(type(f))
+g=(1,"Hello","A",10.0)
+print(type(g))
