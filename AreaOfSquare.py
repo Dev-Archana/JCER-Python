753361b65715823603f01c3930e80a2694d485bd
Archana <167700685+Dev-Archana@users.noreply.github.com>
2024-09-23 10:59:22 +0530

    Create AreaOfSquare.py

---
+++ AreaOfSquare.py
@@ -0,0 +1,11 @@
+#Program to find the area of the square for a given side value
+#reading the values from end user
+side=int(input("Enter Side of square :"))
+area=side*side
+print("Area of square :",area)
+
+#Method 2-Static value
+side=4
+area=side*side
+print(area)
+
